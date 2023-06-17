60d03f959c11b71c0817dde2876bd7e96227f9de
Yashji010 <yk.3691200@gmail.com>
2023-06-17 15:02:47 +0530

    First Commit
    First commit

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    
+    $("#project1").hover(function(){
+        $("#project1name").css({"opacity": "0.5"});  
+      },
+      function(){
+        $("#project1name").css({"opacity": "1"});  
+      });
+
+    $("#project2").hover(function(){
+        $("#project2name").css({"opacity": "0.5"});  
+      },
+      function(){
+        $("#project2name").css({"opacity": "1"});  
+      });
+
+    $("#project3").hover(function(){
+        $("#project3name").css({"opacity": "0.5"});  
+      },
+      function(){
+        $("#project3name").css({"opacity": "1"});  
+      });
+  
+}); 
